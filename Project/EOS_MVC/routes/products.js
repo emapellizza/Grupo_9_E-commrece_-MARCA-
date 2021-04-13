24112e88d27f52e5bda5ea9a22c79930ffb5f07e
marianopedrini <pedrini.mariano96@gmail.com>
2021-04-13 19:49:06 -0300

    commit

--- Project/EOS_MVC/routes/products.js
+++ Project/EOS_MVC/routes/products.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-
 const productsController = require("../controllers/productsController");
-const validateRegister = require("../middlewares/valRegProduct")
+const validateRegister = require("../middlewares/valRegProduct");
 const uploadProduct = require("../middlewares/multerMiddProduct");
 
 // Listado de productos
@@ -17,11 +16,11 @@ router.get("/detail/:idProduct", productsController.show);
 
 // Crear un nuevo producto:
 router.get("/new", productsController.newProduct);
-router.post("/new",uploadProduct.single("imagenProducto"),validateRegister,productsController.saveProduct);
-
-//Rutas para actualizar un producto:
-router.get("/:idProduct", productsController.show);
-
-// Aca va la ruta a una nueva pagina "producto a actualizar" o algo asi
+router.post(
+  "/new",
+  uploadProduct.single("imagenProducto"),
+  validateRegister,
+  productsController.saveProduct
+);
 
 module.exports = router;
