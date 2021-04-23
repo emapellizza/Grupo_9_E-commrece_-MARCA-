f37cb01e5723e7fbc6964614bbfc3b1292debd41
marianopedrini <pedrini.mariano96@gmail.com>
2021-04-23 18:40:09 -0300

    first commit

--- Project/EOS_MVC/public/js/productValidator.js
+++ Project/EOS_MVC/public/js/productValidator.js
@@ -17,30 +17,6 @@ window.addEventListener("load", function () {
 
   form.addEventListener("submit", function (e) {
     // Validacion campos vacíos
-    let errores = [1, 2, 3, 4, 5, 6];
-
-    if (modelo.value.length < 2) {
-      errores.push("Ingresar un modelo válido");
-      modelo.style.border = "1px solid red";
-    }
-    if (shortDescription.value.length < 10) {
-      errores.push("Ingresar una descripción válida");
-      shortDescription.style.border = "1px solid red";
-    }
-
-    if (errores.length > 0) {
-      body.classList.toggle("grey");
-      for (let i = 0; i < errores.length; i++) {
-        divErrores.innerHTML = "<p>" + errores[i] + "</p>";
-      }
-      e.preventDefault();
-    }
+    e.preventDefault();
   });
-
-  //   window.addEventListener("keypress", function (e) {
-  //     let key = e.key;
-  //     if (key == "x") {
-  //       alert(campos);
-  //     }
-  //   });
 });
