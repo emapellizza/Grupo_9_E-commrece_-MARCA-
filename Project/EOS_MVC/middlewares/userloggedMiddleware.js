0f3d4237249482919a52923088b9d0b418359bc0
emapellizza <emapellizza@gmail.com>
2021-05-02 16:53:17 -0300

    cookies

--- Project/EOS_MVC/middlewares/userloggedMiddleware.js
+++ Project/EOS_MVC/middlewares/userloggedMiddleware.js
@@ -1,7 +1,17 @@
+const tablaJson = require("../data/jsonManager");
+const usersJson = tablaJson("users");
+
 function userLoggedMiddleware(req,res,next){
     res.locals.isLogged == false;
     res.locals.isAdminLogged == false;
-
+   
+    let cookieEmail = req.cookies.userEmail;
+    let userFromCookie = usersJson.findByField("email",cookieEmail);
+    
+    if(userFromCookie){
+        req.session.userLogged = userFromCookie;
+    }
+    
     if(req.session.userLogged){
       
         res.locals.isLogged= true;
